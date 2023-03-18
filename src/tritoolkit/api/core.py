1d3a64f50a6b824652da389e0e7eede174841247
renae-r <rodge103@umn.edu>
2023-03-18 00:16:42 -0500

    api and geography modules working

--- src/tritoolkit/api/core.py
+++ src/tritoolkit/api/core.py
@@ -7,6 +7,8 @@ import pandas as pd
 import requests
 from requests.models import Response
 from joblib import Parallel, delayed
+from tqdm import tqdm
+
 from ..__version__ import __version__
 from .exceptions import (
     TransientTriApiException,
@@ -35,9 +37,9 @@ def retry_on_transient_error(func):
 class TriApiClient:
     def __init__(
         self,
-        base_url: str = "https://data.epa.gov/efservice",
         num_retries: int = 3,
         session: Optional[requests.Session] = None,
+        base_url: Optional[str] = "https://data.epa.gov/efservice",
     ):
         self.num_retries = num_retries
         self.base_url = base_url
@@ -56,9 +58,9 @@ class TriApiClient:
             raise TriApiError(f"An error occured: {http_err}")
 
     def get(self, *args, **kwargs) -> requests.Response:
-        """GET a request from the IPUMS API"""
+        """GET a request from the TRI API"""
         return self.request("get", *args, **kwargs)
-
+        
 
 class Table(TriApiClient):
     def __init__(
@@ -73,35 +75,21 @@ class Table(TriApiClient):
             self._divide_rows_into_chunks(range(0, self.rows + 1), 10000)
         )
 
-    def _get_rows(self):
-        count = self.get(f"{self.table_url}/COUNT/JSON")
+    def _get_rows(self) -> int:
+        count = self.session.get(f"{self.table_url}/COUNT/JSON")
         total_rows = count.json()[0]["TOTALQUERYRESULTS"]
         return total_rows
 
-    def _divide_rows_into_chunks(self, l, n):
+    def _divide_rows_into_chunks(self, l: List, n: int):
         # looping till length l
         for i in range(0, len(l), n):
             l2 = l[i : i + n]
             yield (l2[0], l2[-1])
 
     # In theory I should be able to do all of this with json returns,
-    # but the /JSON option is giving me fits and I don't know why,
+    # but the /JSON option is causing internal server errors and I don't know why,
     # but CSV is working so I am rolling with that for now.
-    def _isolate_csv_header(self, csv_string_list):
-        # grab the first item in the list
-        first = csv_string_list[0]
-        # split it along new lines
-        first_list = first.split("\n")
-        # keep the first item to use as a header
-        head = first_list[0]
-        # now grab everything after the header for
-        # every item in the list
-        body_list = ["\n".join(text.split("\n")[1:]) for text in csv_string_list]
-        body = "".join(body_list)
-        serialized_csv = f"{head}\n{body}"
-        return serialized_csv
-
-    def _csv_string_to_df(self, csv_string):
+    def _csv_string_to_df(self, csv_string: str) -> pd.DataFrame:
         # make it a table
         table_data = StringIO(csv_string)
         # skip bad lines for now
@@ -110,19 +98,17 @@ class Table(TriApiClient):
         table_df = pd.read_csv(table_data, sep=",", on_bad_lines="skip")
         return table_df
 
-    def whole_table(self, url):
-        if self.rows < 10001:
-            df = self.get_row_range(0, self.rows, url)
-        else:
-            # use all but 2 cpu
-            df_segments = Parallel(n_jobs=-3, verbose=1)(
-                delayed(self.get_row_range)(str(i[0]), str(i[1]), url)
-                for i in self.segments
-            )
-            df = pd.concat(df_segments, ignore_index=True)
-        return df
+    def get_row_range(self, row_min: str, row_max: str, url: str = None):
+        """
+        Retrieve a specific set of rows from the table.
 
-    def get_row_range(self, row_min, row_max, url=None):
+        Args:
+            row_min: first row in the range as a string
+            row_max: last row in the range as a string; must be <= row_min + 10000
+            url: optional url specification
+
+        Returns: A pandas data frame with the requested table rows
+        """
         # if url not specified, default standard table url
         if url is None:
             _url = f"{self.table_url}/rows/{row_min}:{row_max}/CSV/"
@@ -136,13 +122,51 @@ class Table(TriApiClient):
                 url_list.append(f"rows/{row_min}:{row_max}/CSV/")
             _url = "/".join(url_list)
         # get table section
-        csv_str = self.get(f"{_url}")
-        csv_str.raise_for_status()
+        csv_str = self.session.get(f"{_url}")
+        # csv_str.raise_for_status()
         # put it into a data frame
         table_df = self._csv_string_to_df(csv_str.text)
         return table_df
+    
+    def all_rows(self, url: Optional[str] = None) -> pd.DataFrame:
+        """
+        Retrieves all rows in the table
+
+        Args:
+            url: optional url specification
+
+        Returns: A pandas data frame containing the entire table.
+        """
+        if self.rows < 10001:
+            df = self.get_row_range(0, self.rows, url)
+        else:
+            # use all but 2 cpu
+            df_segments = Parallel(n_jobs=-3)(
+                delayed(self.get_row_range)(str(i[0]), str(i[1]), url)
+                for i in tqdm(self.segments)
+            )
+            df = pd.concat(df_segments, ignore_index=True)
+        return df
+    
+    def _fix_filter_strings(self, filter_value: str) -> str:
+        # replace spaces with %20
+        fixed_filter_value = filter_value.replace(" ", "%20")
+        # replace dashes with %2D
+        fixed_filter_value = fixed_filter_value.replace("-", "%2D")
+        return fixed_filter_value
 
     def filter(self, filters: Dict[str, Union[str, List[str]]] = {}) -> pd.DataFrame:
+        """
+        Select rows in the table that meet specific conditions. If multiple filter
+        conditions are specified, the returned table will contain only rows for which
+        all conditions are True.
+
+        Args:
+            filters: A dictionary of filter conditions where keys are table column names
+                     and values are table column values to retain.
+
+        Returns: A pandas data frame of the filtered rows
+        """
         # split filters into strings and lists
         single_filters = [f for f in filters.keys() if isinstance(filters[f], str)]
         list_filters = [f for f in filters.keys() if isinstance(filters[f], list)]
@@ -150,18 +174,27 @@ class Table(TriApiClient):
         updated_url = [self.table_url]
         # TODO: better error handling for non-existant columns
         for col in single_filters:
-            updated_url.append(f"{col.upper()}/{filters[col.upper()]}")
+            # fix strings for url
+            filter_value = self._fix_filter_strings(filters[col.upper()])
+            updated_url.append(f"{col.upper()}/{filter_value}")
         updated_url.append("CSV")
         filtered_url = "/".join(updated_url)
-        csv_str = self.get(filtered_url).text
+        csv_str = self.session.get(filtered_url).text
         table_df = self._csv_string_to_df(csv_str)
         # if the table has exactly 10001 records, that means that
         # the request maxed out the number of rows that could be requested
         # in this case, we'll grab and filter the whole table...
-        if len(table_df.index) == 10001:
-            table_df = self.whole_table(filtered_url)
+        # Same deal if the first return is empty (i.e. only list type filters
+        # were specified and none of those values were found in the first return)
+        # This also falls down if bad rows are dropped when the serialized csv is
+        # converted to a data frame...the request is maxed out, but fewer than 10001
+        # rows in the df...
+        if len(table_df.index) == 10001 or len(table_df.index) == 0:
+            table_df = self.all_rows(filtered_url)
         # once we're sure we have all rows required from the single-value filters
         # filter the resulting data frame based on the list filters
         for col in list_filters:
+            # no need to modify filter strings in the list case
+            # as these aren't actually going in a url
             table_df = table_df[table_df[col].isin(filters[col])]
         return table_df
--- src/tritoolkit/geography.py
+++ src/tritoolkit/geography.py
@@ -1,6 +1,17 @@
-from typing import Any
+from typing import Any, Optional
+import warnings
 
+import pandas as pd
 import numpy as np
+import geopandas as gpd
+
+# TODO
+# [] validation for lat and lon fields
+# [] validaiton of TRI points - i.e. are tri facilities in the state and county the TRI data indicates?
+# [] flag mis-matched geographies from above
+
+class MissingCoordinatesWarning(Warning):
+    pass
 
 
 def dms_to_dd(dms_value: Any) -> float:
@@ -26,9 +37,10 @@ def dms_to_dd(dms_value: Any) -> float:
         # if the string less than 7 digits, left pad it
         if len(dms_value) < 7:
             dms_value = dms_value.ljust(7, "0")
-        degrees = int(dms_value[:3])
+        degrees = int(dms_value[1:3])
         minutes = int(dms_value[3:5])
         seconds = int(dms_value[-2:])
+        dd_value = -(degrees + (minutes / 60) + (seconds / 3600))
     else:
         # if the string less than 6 digits, left pad it
         if len(dms_value) < 6:
@@ -36,6 +48,62 @@ def dms_to_dd(dms_value: Any) -> float:
         degrees = int(dms_value[:2])
         minutes = int(dms_value[2:4])
         seconds = int(dms_value[-2:])
-    print(dms_value, degrees, minutes, seconds)
-    dd_value = degrees + (minutes / 60) + (seconds / 3600)
-    return round(dd_value, 8)
+        print(dms_value, degrees, minutes, seconds)
+        dd_value = degrees + (minutes / 60) + (seconds / 3600)
+    return dd_value
+
+
+def to_geopandas_df(df: pd.DataFrame, 
+                    lat_field: str, 
+                    lon_field: str, 
+                    dropna: bool = True):
+    """
+    Convert pandas data frame to geopandas data frame
+    
+    Args:
+        df: pandas data frame containing latitude and longitude fields
+        lat_field: Name of the column containing Latitude in df
+        lon_field: Name of the column containing Longitude in df
+        dropna: drop rows with missing lat or lon values. Default is True
+    
+    Returns:
+        A geopandas data frame using lat_field and lon_field to generate geometry
+    """
+    # TODO: validation of contents of lat and lon field to make sure they are plausibly lat/lon values
+    if dropna:
+        missing_lat = len(df[(df[lat_field].isna() == True)])
+        missing_lon = len(df[(df[lon_field].isna() == True)])
+        df = df[(df[lat_field].isna() == False) & (df[lon_field].isna() == False)]
+        warnings.warn(f"{missing_lat} missing values in {lat_field} and {missing_lon} missing values in {lon_field}. These rows will be dropped",
+                      MissingCoordinatesWarning)
+
+    gdf = gpd.GeoDataFrame(df,
+                           geometry=gpd.points_from_xy(df[lon_field], 
+                                                       df[lat_field]))
+    return gdf
+
+
+def tri_points_to_polygons(gdf: gpd.GeoDataFrame,
+                           shape_file: str) -> gpd.GeoDataFrame:
+    """
+    A convenience method to perform a spatial join of TRI lat/lon point geometries
+    with user-specified polygon geometries.
+
+    Args:
+        gdf: A geopandas data frame that contains point geometry of TRI facilities or release sites.
+        shape_file: Path to a shape file that contains a polygon layer
+
+    Returns: a geopandas GeoDataFrame with polygon geometry for polygons in the shape_file containing TRI points
+    """
+    polygons = gpd.read_file(shape_file)
+    # if gdf crs is not defined, assume these are manual lat/lon, and assign WSG4
+    if gdf.crs is None:
+        gdf = gdf.set_crs(epsg=4326)
+    # set crs of point gdf to match the polygons
+    gdf = gdf.to_crs(polygons.crs)
+    # spatial join - polygons that contain points
+    tri_polygons = gpd.sjoin(polygons, 
+                             gdf, 
+                             how="inner", 
+                             predicate="contains")
+    return tri_polygons
