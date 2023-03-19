e63537f66f9b979da768f753f6e04ecacf16de94
renae-r <rodge103@umn.edu>
2023-03-19 01:50:35 -0500

    add geocoding convenience methods

--- src/tritoolkit/geography.py
+++ src/tritoolkit/geography.py
@@ -4,13 +4,21 @@ import warnings
 import pandas as pd
 import numpy as np
 import geopandas as gpd
+from geopy.extra.rate_limiter import RateLimiter
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderUnavailable
 
 # TODO
 # [] validation for lat and lon fields
 # [] validaiton of TRI points - i.e. are tri facilities in the state and county the TRI data indicates?
 # [] flag mis-matched geographies from above
 
+
 class MissingCoordinatesWarning(Warning):
+    pass
+
+
+class RetriesExhaustedWarning(Warning):
     pass
 
 
@@ -53,19 +61,18 @@ def dms_to_dd(dms_value: Any) -> float:
     return dd_value
 
 
-def to_geopandas_df(df: pd.DataFrame, 
-                    lat_field: str, 
-                    lon_field: str, 
-                    dropna: bool = True):
+def to_geopandas_df(
+    df: pd.DataFrame, lat_field: str, lon_field: str, dropna: bool = True
+):
     """
     Convert pandas data frame to geopandas data frame
-    
+
     Args:
         df: pandas data frame containing latitude and longitude fields
         lat_field: Name of the column containing Latitude in df
         lon_field: Name of the column containing Longitude in df
         dropna: drop rows with missing lat or lon values. Default is True
-    
+
     Returns:
         A geopandas data frame using lat_field and lon_field to generate geometry
     """
@@ -74,20 +81,21 @@ def to_geopandas_df(df: pd.DataFrame,
         missing_lat = len(df[(df[lat_field].isna() == True)])
         missing_lon = len(df[(df[lon_field].isna() == True)])
         df = df[(df[lat_field].isna() == False) & (df[lon_field].isna() == False)]
-        warnings.warn(f"{missing_lat} missing values in {lat_field} and {missing_lon} missing values in {lon_field}. These rows will be dropped",
-                      MissingCoordinatesWarning)
+        warnings.warn(
+            f"{missing_lat} missing values in {lat_field} and {missing_lon} missing values in {lon_field}. These rows will be dropped",
+            MissingCoordinatesWarning,
+        )
 
-    gdf = gpd.GeoDataFrame(df,
-                           geometry=gpd.points_from_xy(df[lon_field], 
-                                                       df[lat_field]))
-    
+    gdf = gpd.GeoDataFrame(
+        df, geometry=gpd.points_from_xy(df[lon_field], df[lat_field])
+    )
+
     # set crs
     gdf.set_crs(epsg=4326, inplace=True)
     return gdf
 
 
-def tri_points_to_polygons(gdf: gpd.GeoDataFrame,
-                           shape_file: str) -> gpd.GeoDataFrame:
+def tri_points_to_polygons(gdf: gpd.GeoDataFrame, shape_file: str) -> gpd.GeoDataFrame:
     """
     A convenience method to perform a spatial join of TRI lat/lon point geometries
     with user-specified polygon geometries.
@@ -105,8 +113,75 @@ def tri_points_to_polygons(gdf: gpd.GeoDataFrame,
     # set crs of point gdf to match the polygons
     gdf = gdf.to_crs(polygons.crs)
     # spatial join - polygons that contain points
-    tri_polygons = gpd.sjoin(polygons, 
-                             gdf, 
-                             how="inner", 
-                             predicate="contains")
+    tri_polygons = gpd.sjoin(polygons, gdf, how="inner", predicate="contains")
     return tri_polygons
+
+
+def geocode_from_raw_address(address_string, geolocator_obj, attempt=1, num_retries=5):
+    geocode = RateLimiter(
+        geolocator_obj.geocode,
+        min_delay_seconds=1,
+        max_retries=0,
+        swallow_exceptions=False,
+    )
+    try:
+        location = geocode(address_string)
+        return (location.latitude, location.longitude)
+    except GeocoderUnavailable:
+        if attempt > num_retries:
+            warnings.warn(
+                f"!! RAN OUT OF RETRIES: {address_string}", RetriesExhaustedWarning
+            )
+        if attempt <= num_retries:
+            return geocode_from_raw_address(
+                address_string, geolocator_obj, attempt=attempt + 1
+            )
+        # raise
+    except AttributeError:
+        # addresses not found returned as none
+        return None
+    except Exception as exec:
+        return np.nan
+
+
+def geocode_wrapper(address_components):
+    geolocator = Nominatim(user_agent="tritoolkit")
+    address_string = " ".join(
+        [
+            address_components["STREET_ADDRESS"],
+            address_components["CITY_NAME"],
+            address_components["COUNTY_NAME"],
+            address_components["STATE_ABBR"],
+            address_components["ZIP_CODE"],
+        ]
+    )
+    location = geocode_from_raw_address(address_string, geolocator)
+    # if location not found, try without county
+    if location is None:
+        address_string = " ".join(
+            [
+                address_components["STREET_ADDRESS"],
+                address_components["CITY_NAME"],
+                address_components["STATE_ABBR"],
+                address_components["ZIP_CODE"],
+            ]
+        )
+        location = geocode_from_raw_address(address_string, geolocator)
+    # if location still not found, try without cardinal directions in street address
+    if location is None and address_components["STREET_ADDRESS"].strip()[-2:] in [
+        "NE",
+        "SE",
+        "NW",
+        "SW",
+    ]:
+        address_string = " ".join(
+            [
+                address_components["STREET_ADDRESS"][:-3],
+                address_components["CITY_NAME"],
+                address_components["COUNTY_NAME"],
+                address_components["STATE_ABBR"],
+                address_components["ZIP_CODE"],
+            ]
+        )
+        location = geocode_from_raw_address(address_string, geolocator)
+    return location
