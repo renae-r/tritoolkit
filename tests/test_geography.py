6328c4cf0aec054a1e56627b7ce601a3edc7dd22
renae-r <rodge103@umn.edu>
2023-03-19 18:54:35 -0500

    add tests for geocoding convenience methods

--- tests/test_geography.py
+++ tests/test_geography.py
@@ -3,6 +3,7 @@ import pickle
 from pathlib import Path
 
 import numpy as np
+from geopy.geocoders import Nominatim
 
 from tritoolkit import geography
 
@@ -16,6 +17,8 @@ def test_dms_to_dd():
     assert geography.dms_to_dd(-324528.0) == -32.757777777777775
 
     assert geography.dms_to_dd(1005305.0) == 100.88472222222222
+
+    assert geography.dms_to_dd(-1005305.0) == -100.88472222222222
 
 
 def test_to_gopandas_df(fixtures_path: Path):
@@ -44,8 +47,35 @@ def test_tri_points_to_polygons(fixtures_path: Path):
     nd_facilities_counties = geography.tri_points_to_polygons(
         nd_facilities_geo_pkl, fixtures_path / "nd_counties_all.zip"
     )
-    print(len(nd_facilities_counties))
-    print(len(nd_counties_facilities_pkl))
-    print(nd_facilities_counties.head())
-    print(nd_counties_facilities_pkl.head())
     assert nd_facilities_counties.equals(nd_counties_facilities_pkl)
+
+
+@pytest.mark.vcr
+def test_geocode_from_raw_address():
+    geolocator = Nominatim(user_agent="test_geocoder")
+    lat, lon = geography.geocode_from_raw_address("7320 MILL RD FLORENCE SC 29506", geolocator)
+    assert (lat, lon) == (34.1941851, -79.586317)
+
+
+@pytest.mark.vcr
+def test_geocode_wrapper():
+    lat, lon = geography.geocode_wrapper({
+        "STREET_ADDRESS": "512 LIBERTY EXPRESSWAY SE",
+        "CITY_NAME": "ALBANY",
+        "COUNTY_NAME": "DOUGHERTY",
+        "STATE_ABBR": "GA",
+        "ZIP_CODE": "31705",
+    })
+    assert (lat, lon) == (31.54827665, -84.10798120068999)
+
+    lat, lon = geography.geocode_wrapper({
+        "STREET_ADDRESS": "7320 MILL RD",
+        "CITY_NAME": "FLORENCE",
+        "COUNTY_NAME": "FLORENCE",
+        "STATE_ABBR": "SC",
+        "ZIP_CODE": "29506",
+    })
+    assert (lat, lon) == (34.1941851, -79.586317)
+
+
+
